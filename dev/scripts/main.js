029e8c2f390ff93ffb5f1a2746e04ee6d1294e0c
Eugene G <e.gapanovich@invento.by>
2023-10-17 16:39:54 +0300

    added scene and field

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -1,3 +1,3 @@
 document.addEventListener('DOMContentLoaded', () => {
-  
+  //=require ./scene-navigation.js
 });
---
+++ dev/scripts/scene-navigation.js
@@ -0,0 +1,36 @@
+const scene = document.getElementById('scene');
+
+let isMouseDown = false;
+
+let startX = 0;
+let startY = 0;
+
+let scrollTop = 0;
+let scrollLeft = 0;
+
+scene.addEventListener('mousedown', (event) => {
+  isMouseDown = true;
+
+  startX = event.pageX - scene.offsetLeft;
+  startY = event.pageY - scene.offsetTop;
+
+  scrollLeft = scene.scrollLeft;
+  scrollTop = scene.scrollTop;
+});
+
+scene.addEventListener('mouseup', () => isMouseDown = false);
+
+scene.addEventListener('mouseleave', () => isMouseDown = false);
+
+scene.addEventListener('mousemove', (event) => {
+  if (!isMouseDown) return;
+
+  const x = event.pageX - scene.offsetLeft;
+  const shiftX = (x - startX) * 2;
+
+  const y = event.pageY - scene.offsetTop;
+  const shiftY = (y - startY) * 2;
+
+  scene.scrollLeft = scrollLeft - shiftX;
+  scene.scrollTop = scrollTop - shiftY;
+});
